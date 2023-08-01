91d15591ea1eed1f80b548e6c2fa76f6a1c2045f
Jingchang Chen <jcchen0619@bitbucket.org>
2023-08-01 18:01:53 +1200

    modify vercel config

--- week3/MERN-app-answers/mern_a_to_z_server/app.js
+++ week3/MERN-app-answers/mern_a_to_z_server/app.js
@@ -15,7 +15,7 @@ app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-app.use('/api/books', books);
+// app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
