d196a6d5eadae001c6616db121bce719b069164e
Jingchang Chen <jingchang.chen@autuni.ac.nz>
2023-08-01 22:01:49 +1200

    update

--- week3/MERN-app-answers/mern_a_to_z_client/src/components/ShowBookList.js
+++ week3/MERN-app-answers/mern_a_to_z_client/src/components/ShowBookList.js
@@ -9,7 +9,7 @@ function ShowBookList() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:8082/api/books')
+      .get(process.env.SERVER + '/api/books')
       .then((res) => {
         setBooks(res.data);
       })
