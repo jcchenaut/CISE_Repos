7455adafc9eb6295ed1a29b39ff200b5c6ddb057
Jingchang Chen <jingchang.chen@autuni.ac.nz>
2023-08-07 20:15:04 +1200

    update

--- week3/MERN-app-answers/mern_a_to_z_server/app.js
+++ week3/MERN-app-answers/mern_a_to_z_server/app.js
@@ -1,27 +1,26 @@
-const path = require("path")
-process.env['NODE_CONFIG_DIR'] = path.join(path.resolve("./"),"config/")
 const express = require('express');
 const connectDB = require('./config/db');
-
 const cors = require('cors');
 
-const app = express();
-
+// routes
 const books = require('./routes/api/books');
+
+const app = express();
 
 // Connect Database
 connectDB();
 
+// cors
 app.use(cors({ origin: true, credentials: true }));
 
+// Init Middleware
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
+// use Routes
 app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-// module.exports = app;
